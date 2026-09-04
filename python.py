2cfcf22567989f4eedfc7e397651e9640b76865a
qaranbabmakah-source <qaran_babmakah@hotmail.com>
2026-09-04 10:51:41 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,56 @@
+
+
+tall1 = 10
+tall2 = 5
+
+sum = tall1 + tall2
+produkt = tall1 * tall2
+
+print(sum)
+
+
+print(produkt)
+
+navn = input("Hva heter du? ")
+
+print("Hei", navn)
+
+
+bosted = input("Hvor bor du? ")
+
+print("Hei", navn, "du bor i", bosted)
+
+alder = int(input("Hvor gammel er du? "))
+
+print("Om fem år er du", alder + 5)
+alder = int(input("Hvor gammel er du? "))
+
+print("Om 5 år er du", alder + 5, "år")
+print("Om 10 år er du", alder + 10, "år")
+print("Om 20 år er du", alder + 20, "år")
+alder = int(input("Hvor gammel er du? "))
+aar = int(input("Hvor mange år fram i tid? "))
+
+print("Om", aar, "år er du", alder + aar, "år")
+
+tall1 = int(input("Skriv inn første tall: "))
+tall2 = int(input("Skriv inn andre tall: "))
+
+sum = tall1 + tall2
+
+print("Summen er", sum)
+tall1 = float(input("Skriv inn første tall: "))
+tall2 = float(input("Skriv inn andre tall: "))
+
+tall1 = float(input("Skriv inn første tall: "))
+tall2 = float(input("Skriv inn andre tall: "))
+
+sum = tall1 + tall2
+differanse = tall1 - tall2
+produkt = tall1 * tall2
+gjennomsnitt = (tall1 + tall2) / 2
+
+print("Summen er", sum)
+print("Differansen er", differanse)
+print("Produktet er", produkt)
+print("Gjennomsnittet er", gjennomsnitt)
